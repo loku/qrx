29bb84100798695c507028a7ff79e41c315c50f1
Roger Castillo <roger.castillo@loku.com>
2012-05-15 15:41:14 -0500

    refectors to use better prototyping added stats and improved testing

--- test/channel-spec.js
+++ test/channel-spec.js
@@ -4,24 +4,25 @@ var expect = require('chai').expect,
     Rx = require('rx').Rx,
     uuid = require('node-uuid');
 
-var Channel  = require('../lib/channels.js').Channel;
+var Channel  = require('../lib/channels.js');
 
 
 describe('channels', function(){
   it('should be able to send messages and receive them by subscription', function(){
-    var testChannel = new Channel(uuid.v1());
+    var testChannel = new Channel();
     var deferredMessage = Q.defer();
     var deferredClose = Q.defer();
-    testChannel.asObservable().Subscribe(function(message) {
+    
+    testChannel.subscribe(function(message) {
       expect(message).to.equal('foo');
       testChannel.close();
       deferredMessage.resolve(message);
     },
-    function(exn){},
-    function(){
+    function(exn) {},
+    function() {
       deferredClose.resolve(true);
     });
-    return Q.all([Q.node(testChannel.sendMessage, testChannel, 'foo')(), 
+    return Q.all([Q.node(testChannel.send, testChannel, 'foo')(), 
                   deferredMessage.promise,
                   deferredClose.promise]);
   })
