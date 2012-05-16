7ad0fc83146cd2ba6670b27ba14abab20fb5b72c
Roger Castillo <roger.castillo@loku.com>
2012-05-16 00:17:54 -0500

    fixed export

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-exports.WorkQueueRx  = require('./lib/qrx.js').WorkQueueRx;
-exports.extendRx = require('./lib/rx-extensions.js').extendRx;
+exports.Qrx  = require('./lib/qrx');
+exports.extendRx = require('./lib/rx-extensions').extendRx;
